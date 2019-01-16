2af5a98e11767deb6333b5c5ca76c1632c76a1d8
Mohit Choudhary <choudharymohit1109@gmail.com>
2019-01-16 22:58:19 +0530

    Attendance Complete

--- app/src/main/java/com/example/mohit/payroll/AttendanceActivity.java
+++ app/src/main/java/com/example/mohit/payroll/AttendanceActivity.java
@@ -2,12 +2,28 @@ package com.example.mohit.payroll;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class AttendanceActivity extends AppCompatActivity {
 
+    LinearLayout ll_out_button;
+    LinearLayout ll_in_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendance);
+
+        ll_out_button = findViewById(R.id.ll_out_button);
+        ll_in_button = findViewById(R.id.ll_in_button);
+        ll_in_button.setVisibility(View.VISIBLE);
+        ll_out_button.setVisibility(View.GONE);
+        ll_in_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ll_in_button.setVisibility(View.GONE);
+                ll_out_button.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mohit/payroll/Dashboard_activity.java
+++ app/src/main/java/com/example/mohit/payroll/Dashboard_activity.java
@@ -1,13 +1,26 @@
 package com.example.mohit.payroll;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class Dashboard_activity extends AppCompatActivity {
 
+    LinearLayout ll_attendance;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_activity);
+
+        ll_attendance = findViewById(R.id.ll_attendance);
+        ll_attendance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AttendanceActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
